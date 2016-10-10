a3ef0c27ef25cb15a60cd74fce79cdf3e9a3905a
Aaron Morris <aaronmorris@connect.glos.ac.uk>
2016-10-10 17:27:49 +0100

    Added comic book repository

---
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[] 
+        {
+            new ComicBook()
+            {
+                SeriesTitle = "The amazing Spider Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                    {
+                        new Artist() { Name = "dan slott", Role = "script" },
+                        new Artist() { Name = "humberto ramos", Role = "pencils" },
+                        new Artist() { Name = "victor olazaba", Role = "inks" },
+                        new Artist() { Name = "edgar delgado", Role = "colors" },
+                        new Artist() { Name = "chris eliopoulos", Role = "letters" }
+                    }
+
+            }
+        };
+
+        public ComicBook GetComicBook()
+        {
+
+            return comicBook;
+        }
+    }
+}
